1343d4499c9359937dab9b4ad2f1f4d020c1357d
BrianLiang938 <brian.g.liang938@gmail.com>
2022-06-14 17:07:07 -0400

    python dp

---
+++ python/deleteOperationTwoStrings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        dp = [[0] * (len(word2) + 1) for _ in range(len(word1) + 1)]
+        for i, c in enumerate(word1):
+            for j, d in enumerate(word2):
+                dp[i + 1][j + 1] = 1 + dp[i][j] if c == d else max(dp[i][j + 1], dp[i + 1][j])
+        return len(word2) + len(word1) - 2 *dp[-1][-1]
